54632aaa970ed671e3819c5d680db1d18dd95490
Jesusfollower <42250375+Jesusfollower@users.noreply.github.com>
2019-05-23 17:48:03 +0200

    Add files via upload

---
+++ Scheduling_ITRW316/Algo.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data;
+
+namespace CpuScheduling
+{
+    class Algo
+    {
+        TextBox output;
+        List<Process> Grant_Chart = new List<Process>();
+        My_CPU cpu = new My_CPU();
+
+        
+        public Algo(TextBox tb)
+        {
+            this.output = tb;
+            
+        }
+
+        public static string[] SpritString(String line)
+        {
+            char[] delimeter = { ' ', '\t' };
+            string[] word = line.Split(delimeter, StringSplitOptions.RemoveEmptyEntries);
+            return word;
+
+        }
+
+        public bool StillProcessToWork(List<Process> processList)
+        {
+            int n = processList.Count;
+            for (int i = 0; i < n; i++)
+            {
+                if (!processList[i].IsFinish)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+
+       
+
+       
+        
+        void swapProcesses(int i, int i2, List<Process> processList)
+        {
+            Process temp = processList[i];
+            processList[i] = processList[i2];
+            processList[i2] = temp;
+        }
+
+
+        public void SortProcess(String by, List<Process> processes)
+        {
+            int j, i;
+            int n = processes.Count;
+            switch (by)
+            {
+                case "byArriveTime":
+                    for (j = 0; j < n; j++)
+                        for (i = 0; i <n - 1; i++)
+                            if (processes[i].arriveTime > processes[i + 1].arriveTime)
+                                swapProcesses(i, i + 1, processes);
+                      break;
+
+
+                case "byBurstTime":
+                    for (j = 0; j < n; j++)
+                        for (i = 0; i < n - 1; i++)
+                        {
+                            if (processes[i].burstTime == processes[i + 1].burstTime)
+                                if (processes[i].arriveTime > processes[i + 1].arriveTime)
+                                    swapProcesses(i, i + 1, processes);
+                            if (processes[i].burstTime > processes[i + 1].burstTime)
+                                swapProcesses(i, i + 1,processes);
+                        }
+               
+                    break;
+
+                case "byRemainingTime":
+                    for (j = 0; j < n; j++)
+                        for (i = 0; i < n - 1; i++)
+                        {
+                            if (processes[i].time_remaining == processes[i + 1].time_remaining)
+                                if (processes[i].arriveTime > processes[i + 1].arriveTime)
+                                    swapProcesses(i, i + 1,processes);
+                            if (processes[i].time_remaining > processes[i + 1].time_remaining)
+                                swapProcesses(i, i + 1,processes);
+                        }
+                    
+                    break;
+
+                case "byPriority":
+                    for ( j = 0; j < n; j++)
+                        for ( i = 0; i < n - 1; i++)
+                        {
+                            if (processes[i].priority == processes[i + 1].priority)
+                                if (processes[i].arriveTime > processes[i + 1].arriveTime)
+                                    swapProcesses(i, i + 1,processes);
+                            if (processes[i].priority > processes[i + 1].priority)
+                                swapProcesses(i, i + 1,processes);
+                        }
+                   
+                    break;
+                
+                case "byName":
+                    for (j = 0; j < n; j++)
+                        for (i = 0; i < n - 1; i++)
+                            if (processes[i].getName() > processes[i + 1].getName())
+                                swapProcesses(i, i + 1,processes);
+                    break;
+                  
+            }
+     
+            
+        }
+
+        int getProcessIndexByName(String name,List<Process> processList)
+        {
+            int n = processList.Count;
+            int index = -1;
+            for (int i = 0; i < n; i++)
+                if (processList[i].name == name)
+                {
+                    index = i;
+                    break;
+                }
+            return index;
+        }
+
+        public void preemtivePriority(List<Process> processList)
+        {
+            Grant_Chart.Clear();
+            int time, currentIndex, bigValue = 100000;
+            int n = processList.Count;
+           
+            SortProcess("byPriority",processList);
+            cpu.priority = bigValue;
+            for (time = 0; StillProcessToWork(processList); time++)
+            {
+                
+                SortProcess("byPriority", processList);
+                for (int i = 0; i < n; i++)
+                {
+                  
+                    if (!processList[i].IsFinish && processList[i].arriveTime <= time && processList[i].priority <= cpu.priority)
+                    {
+                       
+                        if (!cpu.IsIdle)
+                        {
+                            if (i == getProcessIndexByName(cpu.Inside_Process.name, processList))
+                            {
+                                break;
+                            }
+                        }
+
+                        if (processList[i].start_time == -1)
+                            processList[i].start_time = time;
+                   
+                        processList[i].putIntimes.Add(time);
+                        Grant_Chart.Add(processList[i]);
+                        cpu.Inside_Process = processList[i];
+                        cpu.IsIdle = false;
+                       cpu.priority = processList[i].priority;
+                    
+                        break;
+                    }
+                }
+                if (!cpu.IsIdle)
+                {                 
+                    currentIndex = getProcessIndexByName(cpu.Inside_Process.name,processList);                    
+                }                      
+                else
+                    continue;
+             
+                processList[currentIndex].decrementTimeRemaining(1);
+                
+                if (processList[currentIndex].time_remaining == 0)
+                {
+                  
+                    processList[currentIndex].finish_time = time+1;
+                    processList[currentIndex].IsFinish = true;
+                    processList[currentIndex].waiting_time =processList[currentIndex].finish_time - processList[currentIndex].burstTime - processList[currentIndex].arriveTime;
+                    processList[currentIndex].CalculateTurnAroundTime();
+                    cpu.priority = bigValue;
+                    cpu.IsIdle = true;
+                }
+             
+                
+            }
+            SortProcess("byName", processList);
+            Conclusion(processList);
+        }
+
+        int GetTotalCPUTime(List<Process> processList)
+        {
+            int TotalProcessingTime = 0;
+            for (int i = 0; i < processList.Count; i++)
+                TotalProcessingTime += processList[i].burstTime;
+
+            return TotalProcessingTime;
+        }
+
+        public void RR(List<Process> processList, int quantum)
+        {
+            Grant_Chart.Clear();
+            int time = 0;
+            cpu.IsIdle = true;
+            SortProcess("byArriveTime", processList);
+          
+            int  q = 0;
+        
+            while (StillProcessToWork(processList))
+            {
+                for (int i = 0; i < processList.Count; i++)
+                {
+                    if (q == 0 && !processList[i].IsFinish && cpu.IsIdle && processList[i].arriveTime <= time)
+                    {
+                        if (processList[i].start_time == -1)
+                            processList[i].start_time = time;
+                        processList[i].putIntimes.Add(time);
+                        Grant_Chart.Add(processList[i]);
+                      
+                        
+                        cpu.IsIdle = false;
+                       cpu.Inside_Process = processList[i];
+                    }
+                    if (cpu.IsIdle)
+                    {
+                       
+                        continue;
+                    }
+
+                    for (; StillProcessToWork(processList);time++ )
+                    {
+                        processList[getProcessIndexByName(cpu.Inside_Process.name,processList)].decrementTimeRemaining(1);
+                     
+                        if (processList[getProcessIndexByName(cpu.Inside_Process.name,processList)].time_remaining == 0)
+                        {
+                            time++;
+                            q = 0;
+                            cpu.IsIdle = true;
+                            int currentIndex = getProcessIndexByName(cpu.Inside_Process.name,processList);
+                            processList[currentIndex].finish_time = time;
+                            processList[currentIndex].waiting_time = processList[currentIndex].finish_time - processList[currentIndex].burstTime - processList[currentIndex].arriveTime;
+                            processList[currentIndex].CalculateTurnAroundTime();
+                            processList[currentIndex].IsFinish = true;
+                        
+                            
+                            break;
+                        }
+                        q++;
+                        if (q == quantum)
+                        {
+                            q = 0;
+                           cpu.IsIdle = true;
+                            time++;
+                            break;
+                        }
+                      
+                    }
+                }
+            }
+            
+            SortProcess("byName", processList);
+            Conclusion(processList);
+            
+        }
+
+        void RollBackTimeRemaining(List<Process> ProcessList)
+        {
+            for (int i = 0; i < ProcessList.Count; i++)
+            {
+                ProcessList[i].time_remaining = ProcessList[i].burstTime;
+
+            }
+
+        }
+
+
+       public void showRR()
+        {
+            int i= 3;
+            Process p = Grant_Chart[i];
+            foreach (int a in Grant_Chart[i].putIntimes)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+       public void Conclusion(List<Process> processList)
+       {
+           output.Text = "Process Name" + "\t" + "Waiting Time" + "\t" + "TurnAround Time";
+           output.AppendText("\n");
+           int totalWait = 0;
+           int totalTurn = 0;
+           foreach (Process p in processList)
+           {
+               totalWait += p.waiting_time;
+               totalTurn+=p.turnaround_time;
+               output.AppendText(p.name + "\t\t    " + p.waiting_time + "\t\t    " + p.turnaround_time);
+               output.AppendText("\n");
+           }
+           double n = processList.Count;
+           double averageWait = totalWait / n;
+           double aveageTurn = totalTurn / n;
+           output.AppendText("Ave WT" + averageWait+"\n");
+           output.AppendText("Ave TT= " + aveageTurn+"\n");  
+       }
+
+        public DataTable showGrantChart()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Process", typeof(String));
+            table.Columns.Add("In", typeof(int));
+            table.Columns.Add("Out", typeof(int));
+          
+            RollBackTimeRemaining(Grant_Chart);
+           
+            for (int i = 0; i < Grant_Chart.Count; i++)
+            {
+                int count = Grant_Chart[i].countIntime;
+                
+
+                if (i < Grant_Chart.Count - 1)
+                {
+                    int count2 = Grant_Chart[i + 1].countIntime;
+
+                    int burstTimeNow = Grant_Chart[i + 1].putIntimes[count2] - Grant_Chart[i].putIntimes[count];
+                    int timeRemaining = Grant_Chart[i].time_remaining - burstTimeNow;
+                    
+
+                    if (timeRemaining >= 0)
+                    {
+                        if (Grant_Chart[i].name != Grant_Chart[i + 1].name)
+                        {
+                            Console.WriteLine(Grant_Chart[i].name + " " + Grant_Chart[i].putIntimes[count] + " " + Grant_Chart[i + 1].putIntimes[count2] + " " + count2);
+                            Grant_Chart[i].time_remaining = timeRemaining;
+                            table.Rows.Add(Grant_Chart[i].name, Grant_Chart[i].putIntimes[count], Grant_Chart[i + 1].putIntimes[count2]);
+                        }
+                        else
+                        {
+                            Console.WriteLine(Grant_Chart[i].name + " " + Grant_Chart[i].putIntimes[count] + " " + Grant_Chart[i + 1].putIntimes[count+1] );
+                            table.Rows.Add(Grant_Chart[i].name, Grant_Chart[i].putIntimes[count], Grant_Chart[i + 1].putIntimes[count+1]);
+                        }
+                    }
+                    else
+                    {
+                        table.Rows.Add(Grant_Chart[i].name, Grant_Chart[i].putIntimes[count], (Grant_Chart[i].putIntimes[count] + Grant_Chart[i].time_remaining));
+                        Grant_Chart[i].time_remaining = 0;
+                    }
+                }
+                else
+                {
+                    table.Rows.Add(Grant_Chart[i].name, Grant_Chart[i].putIntimes[count], Grant_Chart[i].finish_time);
+                }
+                Grant_Chart[i].countIntime++;
+            }
+            return table;
+        }
+
+        
+    }
+}
---
+++ Scheduling_ITRW316/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace CpuScheduling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_Browse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.bt_OK = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groubBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tb_quantum = new System.Windows.Forms.TextBox();
+            this.rd_bt_rr = new System.Windows.Forms.RadioButton();
+            this.rd_bt_pp = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groubBox3.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt_Browse
+            // 
+            this.bt_Browse.Location = new System.Drawing.Point(6, 178);
+            this.bt_Browse.Name = "bt_Browse";
+            this.bt_Browse.Size = new System.Drawing.Size(147, 26);
+            this.bt_Browse.TabIndex = 0;
+            this.bt_Browse.Text = "Get Process";
+            this.bt_Browse.UseVisualStyleBackColor = true;
+            this.bt_Browse.Click += new System.EventHandler(this.bt_Browse_Click);
+            // 
+            // bt_OK
+            // 
+            this.bt_OK.Location = new System.Drawing.Point(6, 159);
+            this.bt_OK.Name = "bt_OK";
+            this.bt_OK.Size = new System.Drawing.Size(75, 23);
+            this.bt_OK.TabIndex = 2;
+            this.bt_OK.Text = "OK";
+            this.bt_OK.UseVisualStyleBackColor = true;
+            this.bt_OK.Click += new System.EventHandler(this.bt_OK_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 15);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(469, 157);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(8, 19);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(648, 246);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.bt_Browse);
+            this.groupBox1.Location = new System.Drawing.Point(18, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(498, 210);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Process";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.groupBox4);
+            this.groupBox2.Location = new System.Drawing.Point(18, 237);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(679, 296);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Simulation";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dataGridView2);
+            this.groupBox4.Location = new System.Drawing.Point(6, 19);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(665, 271);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Grant Chart";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.textBox1);
+            this.groupBox5.Location = new System.Drawing.Point(715, 21);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(466, 255);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Conclusion";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 234);
+            this.textBox1.TabIndex = 6;
+            // 
+            // groubBox3
+            // 
+            this.groubBox3.Controls.Add(this.groupBox3);
+            this.groubBox3.Controls.Add(this.bt_OK);
+            this.groubBox3.Location = new System.Drawing.Point(537, 43);
+            this.groubBox3.Name = "groubBox3";
+            this.groubBox3.Size = new System.Drawing.Size(160, 188);
+            this.groubBox3.TabIndex = 8;
+            this.groubBox3.TabStop = false;
+            this.groubBox3.Text = "Schedule Algorithms";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tb_quantum);
+            this.groupBox3.Controls.Add(this.rd_bt_rr);
+            this.groupBox3.Controls.Add(this.rd_bt_pp);
+            this.groupBox3.Location = new System.Drawing.Point(20, 28);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(132, 122);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Choose:";
+            // 
+            // tb_quantum
+            // 
+            this.tb_quantum.Enabled = false;
+            this.tb_quantum.Location = new System.Drawing.Point(104, 72);
+            this.tb_quantum.Name = "tb_quantum";
+            this.tb_quantum.Size = new System.Drawing.Size(19, 20);
+            this.tb_quantum.TabIndex = 9;
+            this.tb_quantum.Text = "4";
+            this.tb_quantum.TextChanged += new System.EventHandler(this.tb_quantum_TextChanged);
+            // 
+            // rd_bt_rr
+            // 
+            this.rd_bt_rr.AutoSize = true;
+            this.rd_bt_rr.Location = new System.Drawing.Point(17, 72);
+            this.rd_bt_rr.Name = "rd_bt_rr";
+            this.rd_bt_rr.Size = new System.Drawing.Size(88, 17);
+            this.rd_bt_rr.TabIndex = 8;
+            this.rd_bt_rr.TabStop = true;
+            this.rd_bt_rr.Text = "Round-Robin";
+            this.rd_bt_rr.UseVisualStyleBackColor = true;
+            this.rd_bt_rr.CheckedChanged += new System.EventHandler(this.rd_bt_rr_CheckedChanged);
+            // 
+            // rd_bt_pp
+            // 
+            this.rd_bt_pp.AutoSize = true;
+            this.rd_bt_pp.Location = new System.Drawing.Point(17, 19);
+            this.rd_bt_pp.Name = "rd_bt_pp";
+            this.rd_bt_pp.Size = new System.Drawing.Size(106, 17);
+            this.rd_bt_pp.TabIndex = 7;
+            this.rd_bt_pp.TabStop = true;
+            this.rd_bt_pp.Text = "Priority-Preemtive";
+            this.rd_bt_pp.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 545);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groubBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CPU Scheduling Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groubBox3.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_Browse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button bt_OK;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groubBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rd_bt_rr;
+        private System.Windows.Forms.RadioButton rd_bt_pp;
+        private System.Windows.Forms.TextBox tb_quantum;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
